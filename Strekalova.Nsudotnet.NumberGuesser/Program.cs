a5d3b13eea996cc95eadc12cedf599ca370b7cbb
Svetlana Strekalova <svetlana.strekalova@gmail.com>
2015-05-12 17:13:24 +0600

    Added task 1
    Added solution for task "NumberGuesser"

---
+++ Strekalova.Nsudotnet.NumberGuesser/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Strekalova.Nsudotnet.NumberGuesser
+{
+    class Program
+    {
+        private static readonly string[] Phrases = { "Знаешь, {0}, угадывальщик из тебя так себе.", "{0}, хватит дурью маяться, иди пиши код.", "Сегодня явно не твой день, {0}.",
+                                               "{0}, мне кажется, что удача от тебя отвернулась.", "{0}, серьезно? Ты же опять не угадал!",
+                                               "Даже мой кот уже догадался, {0}, какое здесь было число."};
+
+        private const string DefaultName = "Странник";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветствую тебя, странник! Как называть тебя?");
+            var s = Console.ReadLine();
+            var username = (string.IsNullOrEmpty(s)) ? DefaultName : s;
+
+            var rand = new Random();
+            var number = rand.Next(0, 101);
+
+            Console.WriteLine("{0}, я хочу сыграть с тобой в игру. Угадай число от 0 до 100.", username);
+
+            var failsCount = 0;
+            var isGuessed = false;
+            var tryings = new int[1000];
+            var startTime = DateTime.Now;
+            var guessTime = startTime.TimeOfDay;
+
+            while (!isGuessed)
+            {
+                int currentValue;
+                s = Console.ReadLine();
+                if (s == "q")
+                {
+                    Console.WriteLine("Не хочешь угадывать - твоё право :/");
+                    Console.ReadKey();
+                    return;
+                }
+
+                try
+                {
+                    currentValue = int.Parse(s);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("{0}, ты должен ввести целое число.", username);
+                    continue;
+                }
+
+                if (currentValue != number)
+                {
+                    tryings[failsCount] = currentValue;
+                    failsCount++;
+                    Console.WriteLine(currentValue > number ? "Многовато будет." : "Мало, побольше бы.");
+                    if (failsCount % 4 == 0)
+                        Console.WriteLine(Phrases[rand.Next(0, Phrases.Length)], username);
+                }
+                else
+                {
+                    isGuessed = true;
+                    guessTime = DateTime.Now.Subtract(startTime);
+                }
+            }
+
+            Console.WriteLine("Поздравляю! Ты угадал число c {0} попытки! Это заняло у тебя {1} секунд!", failsCount + 1, guessTime.Seconds);
+            for (var i = 0; i < failsCount; i++)
+            {
+                Console.WriteLine(tryings[i] > number ? "{0} было больше, чем нужно" : "{0} было слишком мало",
+                    tryings[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strekalova.Nsudotnet.NumberGuesser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Strekalova.Nsudotnet.NumberGuesser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Strekalova.Nsudotnet.NumberGuesser")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("178dc7a7-fd00-4405-87a3-7c0358386a67")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
