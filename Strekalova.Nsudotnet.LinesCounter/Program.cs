336b58015936d4fc938fde74e1d37af201b46cac
Svetlana Strekalova <svetlana.strekalova@gmail.com>
2015-05-13 00:27:40 +0600

    Added task 2
    Added solution for task LinesCounter

---
+++ Strekalova.Nsudotnet.LinesCounter/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Strekalova.Nsudotnet.LinesCounter
+{
+    class Program
+    {
+        private static string _fileExtension;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("В качестве аргумента программы должно быть указано расширение учитываемых файлов");
+                return;
+            }
+
+            _fileExtension = args[0];
+
+            var totalLinesCount = GetLinesCountInDirectory(new DirectoryInfo(Directory.GetCurrentDirectory()));
+
+            Console.WriteLine(string.Concat("Количество осмысленных строк в файлах формата \"", _fileExtension, "\" в текущей директории и поддиректориях: ", totalLinesCount));
+        }
+
+        static long GetLinesCountInDirectory(DirectoryInfo dir)
+        {
+            long linesCount = 0;
+
+            try
+            {
+                var subdirs = dir.GetDirectories();
+                linesCount += subdirs.Sum(directory => GetLinesCountInDirectory(directory));
+
+                var files = dir.GetFiles(string.Concat("*.", _fileExtension));
+                linesCount += files.Sum(file => GetLinesCountInFile(file));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return linesCount;
+        }
+
+        static long GetLinesCountInFile(FileInfo file)
+        {
+            long linesCount = 0;
+            var regex = new Regex("\\/\\*[^\\/]*[^\\*]*\\*\\/|\\/\\/[^\n]*");
+            using (TextReader reader = new StreamReader(file.OpenRead()))
+            {
+                string currString;
+                bool isComment = false;
+                while ((currString = reader.ReadLine()) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(currString))
+                        continue;
+                    if (isComment)
+                        currString = string.Concat("/*", currString);
+                    currString = regex.Replace(currString, "");
+                    if (currString.Contains("/*"))
+                    {
+                        isComment = true;
+                        currString = string.Concat(currString, "*/");
+                        currString = regex.Replace(currString, "");
+                    }
+                    else
+                        isComment = false;
+                    if (!string.IsNullOrWhiteSpace(currString))
+                        linesCount++;
+                }
+            }
+            return linesCount;
+        }
+    }
+}
---
+++ Strekalova.Nsudotnet.LinesCounter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Strekalova.Nsudotnet.LinesCounter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Strekalova.Nsudotnet.LinesCounter")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("664d3e6c-65c1-490e-af68-2ca6c8c4c4bb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
